5eac1f0fd61e9285599a10b342668a773c0bfe41
yannmarc <chamackstone@gmail.com>
2023-08-08 19:54:59 +0100

    added navbar interactivity

--- public/script/app.js
+++ public/script/app.js
@@ -1,1 +1,26 @@
-console.log("Hello world");
+// mobile hamburger
+
+const menuButton = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.nav-menu');
+const navlinks = document.querySelectorAll('.nav-menu ul li');
+
+let activeButton = null;
+
+
+menuButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle("active-menu");
+})
+
+navlinks.forEach(navLink => {
+    navLink.addEventListener("click", (e) => {
+        navLink.classList.add("active");
+        if (activeButton != null && activeButton != e.currentTarget) {
+            activeButton.classList.remove("active");
+        }
+
+        activeButton = e.currentTarget;
+    })
+});
+
+
+
